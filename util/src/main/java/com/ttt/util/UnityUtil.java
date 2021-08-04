303da4c2454fbb8ef8fee5827707e8149f58031d
tkstk <549233474@qq.com>
2021-08-04 21:05:04 +0800

    降为4.1.1 版本

---
+++ util/src/main/java/com/ttt/util/UnityUtil.java
@@ -0,0 +1,42 @@
+package com.ttt.util;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+public class UnityUtil {
+
+    /**
+     * unity项目启动时的的上下文
+     */
+    private Activity _unityActivity;
+    /**
+     * 获取unity项目的上下文
+     * @return
+     */
+    Activity getActivity(){
+        if(null == _unityActivity) {
+            try {
+                Class<?> classtype = Class.forName("com.unity3d.player.UnityPlayer");
+                _unityActivity = (Activity) classtype.getDeclaredField("currentActivity").get(classtype);
+            } catch (ClassNotFoundException e) {
+
+            } catch (IllegalAccessException e) {
+
+            } catch (NoSuchFieldException e) {
+
+            }
+        }
+        return _unityActivity;
+    }
+
+    /**
+     * Toast显示unity发送过来的内容
+     * @param content           消息的内容
+     * @return                  调用是否成功
+     */
+    public boolean showToast(String content){
+        Toast.makeText(getActivity(),content,Toast.LENGTH_SHORT).show();
+
+        return true;
+    }
+}
