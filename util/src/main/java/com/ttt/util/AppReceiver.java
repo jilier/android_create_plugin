de19890349ebb1f31e6e0d1dd13ff52715ec0b6e
tkstk <549233474@qq.com>
2021-08-12 05:59:45 +0800

    测试开机自启插件

--- util/src/main/java/com/ttt/util/AppReceiver.java
+++ util/src/main/java/com/ttt/util/AppReceiver.java
@@ -3,8 +3,7 @@ package com.ttt.util;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
-import com.unity3d.player.UnityPlayerActivity;
+import android.widget.Toast;
 
 public class AppReceiver extends BroadcastReceiver {
     String act = "android.intent.action.BOOT_COMPLETED";
@@ -15,12 +14,17 @@ public class AppReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        //Toast.makeText(context, intent.getAction(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, intent.getAction(), Toast.LENGTH_SHORT).show();
 
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
         if (act.equals(intent.getAction())){
-            Intent startIndent = new Intent(context, UnityPlayerActivity.class);
+            Intent startIndent = null;
+            try {
+                startIndent = new Intent(context, Class.forName("com.unity3d.player.UnityPlayerActivity"));
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
             startIndent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(startIndent);
         }
